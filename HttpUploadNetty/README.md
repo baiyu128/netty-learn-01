0709864924aa4f4b7fecc2d9b955285a204d3d27
15686911993 <baixixi187@163.com>
2020-01-07 15:47:23 +0800

    upload file

---
+++ HttpUploadNetty/README.md
@@ -0,0 +1,2 @@
+# HttpUploadNetty
+根据netty example 实现的文件上传服务器
---
+++ HttpUploadNetty/src/main/java/server/PathUtil.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.io.File;
+
+public class PathUtil {
+    private static final ClassLoader classLoader = PathUtil.class.getClassLoader();
+
+    public static String getFileBasePath() {
+        String os = System.getProperty("os.name");
+        String basePath;
+        if (os.toLowerCase().startsWith("win")) {
+            basePath = "D:/yxtest/";
+        } else {
+            basePath = "/root/upload_source";
+        }
+        basePath = basePath.replace("/", File.separator);
+        return basePath;
+    }
+
+    public static String getSourcePath(String name) {
+        return classLoader.getResource(name).getPath();
+    }
+}
